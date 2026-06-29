31c10e8eefeb65bf63b60b2b3ab109234f4450b3
henrySilverIX <henrysilver.gold@proton.me>
2026-06-28 22:46:54 -0300

    fix: Arrumei o main.py que estava com o código duplicado

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
-
 from app.routers.aluno_router import router as aluno_router
 
-app = FastAPI(title="Basic Gym Management API")
-app = FastAPI(title="Basic Gym Management API")
 
-app.include_router(aluno_router)
+app = FastAPI(title="Basic Gym Management API")
 app.include_router(aluno_router)
