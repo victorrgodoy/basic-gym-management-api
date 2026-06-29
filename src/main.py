52353bed2a7192d7741bfa0c74d02c73d9cd6785
Victor Godoy <victor.hgodoy@icloud.com>
2026-06-29 12:20:25 -0300

    fix: ajuste pastas

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routers.aluno_router import router as aluno_router
 
+from app.controller.aluno_controller import router as aluno_router
 
 app = FastAPI(title="Basic Gym Management API")
+
 app.include_router(aluno_router)
