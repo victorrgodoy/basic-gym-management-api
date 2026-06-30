fe608eeb05bcc26d4b22aa3f51c8bea172c17ebb
Victor Godoy <victor.hgodoy@icloud.com>
2026-06-30 16:51:11 -0300

    fix: ajuste adm service, controller, repo

--- src/app/controller/administrador_controller.py
+++ src/app/controller/administrador_controller.py
@@ -4,19 +4,24 @@ from app.config.database import get_db
 from app.domain.administrador import Administrador
 from app.request.administrador_request import AdministradorRequest
 from app.service.usuario_service import UsuarioService
+from app.service.administrador_service import AdministradorService
 from app.repository.sqlalchemy.sqlalchemy_usuario_repository import SqlAlchemyUsuarioRepository
+from app.repository.sqlalchemy.sqlalchemy_administrador_repository import SqlAlchemyAdministradorRepository
 from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.request.administrador_request import AdministradorUpdate
 
 router = APIRouter(prefix="/administradores", tags=["Administradores"])
 
+def get_administrador_service(db: Session = Depends(get_db)):
+    return AdministradorService(
+        SqlAlchemyAdministradorRepository(db),
+        UsuarioService(SqlAlchemyUsuarioRepository(db))
+    )
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create(request: AdministradorRequest, db: Session = Depends(get_db)):
+def create(request: AdministradorRequest, service: AdministradorService = Depends(get_administrador_service)):
     try:
-        usuario_repo = SqlAlchemyUsuarioRepository(db)
-        service = UsuarioService(usuario_repo)
-        
         novo_admin = Administrador(
             nome=request.nome,
             email=request.email,
@@ -33,25 +38,19 @@ def create(request: AdministradorRequest, db: Session = Depends(get_db)):
 
 
 @router.get("/")
-def read(db: Session = Depends(get_db)):
-    usuario_repo = SqlAlchemyUsuarioRepository(db)
-    service = UsuarioService(usuario_repo)
-    return service.read(tipo="administrador")
+def read(service: AdministradorService = Depends(get_administrador_service)):
+    return service.read()
 
 @router.put("/{id}", response_model=None)
-def update(id: UUID, dados: AdministradorUpdate, db: Session = Depends(get_db)):
+def update(id: UUID, dados: AdministradorUpdate, service: AdministradorService = Depends(get_administrador_service)):
     try:
-        usuario_repo = SqlAlchemyUsuarioRepository(db)
-        service = UsuarioService(usuario_repo)
-        return service.update_administrador(id, dados)
+        return service.update(id, dados)
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete(id: UUID, db: Session = Depends(get_db)):
+def delete(id: UUID, service: AdministradorService = Depends(get_administrador_service)):
     try:
-        usuario_repo = SqlAlchemyUsuarioRepository(db)
-        service = UsuarioService(usuario_repo)
         service.delete(id)
         return
     except ValueError as e:
